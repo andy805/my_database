4635d682141c44c2c2e2e59ed9e9f9c6637c4c0c
BigPump <garrettvk.dev@gmail.com>
2022-05-03 22:14:02 -0700

    fixing routes delete funtion error

--- back_end/routes/interviewQuestions.router.js
+++ back_end/routes/interviewQuestions.router.js
@@ -2,13 +2,13 @@ import { createRequire } from "module";
 const require = createRequire(import.meta.url);
 const express = require("express");
 
-import { httpCreateInterviewQuestion, httpGetAllInterviewQuestions, httpUpdateQuestion } from "./interviewQuestions.controller.js";
+import { httpCreateInterviewQuestion, httpGetAllInterviewQuestions, httpUpdateQuestion, httpDeleteQuestion } from "./interviewQuestions.controller.js";
 
 const interviewQuestionsRouter = express.Router();
 
 interviewQuestionsRouter.get('/', httpGetAllInterviewQuestions);
 interviewQuestionsRouter.post('/', httpCreateInterviewQuestion);
 interviewQuestionsRouter.put('/:questionId', httpUpdateQuestion);
-interviewQuestionsRouter.delete('/', 'function from controller')
+interviewQuestionsRouter.delete('/', httpDeleteQuestion);``
 
 export default interviewQuestionsRouter
